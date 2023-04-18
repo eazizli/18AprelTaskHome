3364d477f45843c6ed29b0b9033fe6c5f8fe8005
Elvin Ezizli <elvin@example.com>
2023-04-18 12:06:23 +0400

    .

---
+++ 17AprelTaskHome/Models/Product.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _17AprelTaskHome.Models
+{
+    internal class Product
+    {
+        public string Name{ get; set; }
+
+    public    List<Catagory> Catagories { get; set;}=new List<Catagory>();
+    }
+}
---
+++ 17AprelTaskHome/Program.cs
@@ -0,0 +1,22 @@
+﻿using _17AprelTaskHome.Models;
+using Newtonsoft.Json;
+
+internal class Program
+{
+    private static void Main(string[] args)
+    {
+        Catagory catagory = new Catagory()
+        {
+            Name = "Mercedes"
+        };
+        Product product = new Product()
+        {
+            Name="E240"
+        };
+
+       
+        
+        string json=JsonConvert.SerializeObject(product);   
+        
+    }
+}
